868bc414fd4727a6d37f54d8d08c8264fd94db04
ollieiscool <oliverperrin10@hotmail.co.uk>
2024-08-03 23:59:17 +0100

    Unit formation Prep
    Preparations for formation logic

---
+++ Source/Diss/FormationClass.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "FormationClass.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DISS_API UMyClass : public UObject
+{
+	GENERATED_BODY()
+	
+};
